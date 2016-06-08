8d3d9677d173176d6a95fa752d3aa6074ef7ad58
Dmitry Sadovnikov <d.sadovnikov@yahoo.com>
2016-06-08 11:44:08 +0300

    add device authentication,  user model, user roles

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  scope '/admin' do
+    resources :users
+  end
   resources :rus_sentences
   resources :eng_sentences
   resources :eng_words
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+['registered', 'banned', 'moderator', 'admin'].each do |role|
+  Role.find_or_create_by({name: role})
+end
