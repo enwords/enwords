ec4f91e20123cc7485766387a8ccd02c6bc3817c
Dmitry Sadovnikov <d.sadovnikov@yahoo.com>
2016-06-09 16:47:46 +0300

    add migrations and add seeds

--- db/migrate/20160609085814_create_word_collections.rb
+++ db/migrate/20160609085814_create_word_collections.rb
@@ -1,6 +1,7 @@
 class CreateWordCollections < ActiveRecord::Migration[5.0]
   def change
     create_table :word_collections do |t|
+      t.string :name, null: false, default: t.timestamps.to_s
       t.references :user, foreign_key: true
 
       t.timestamps
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
 
-EngSentence.new {
-  id: => 'Hello my name is dima'
 
-}
+EngWord.create! [
+                    {id: 1, word: 'is'},
+                    {id: 2, word: 'my'},
+                    {id: 3, word: 'go'},
+                    {id: 4, word: 'hello'},
+                    {id: 5, word: 'name'},
+                    {id: 6, word: 'to'},
+                    {id: 7, word: 'home'},
+                    {id: 8, word: 'baby'},
+                    {id: 9, word: 'the'},
+                    {id: 10, word: 'bad'}
+
+                ]
+
+EngSentence.create! [
+                        {id: 1, sentence: 'Hello my name is Dima', is_audio: false},
+                        {id: 2, sentence: 'Go to home baby', is_audio: true},
+                        {id: 3, sentence: 'His car is bad', is_audio: false},
+                        {id: 4, sentence: 'I work alone', is_audio: true},
+                        {id: 5, sentence: 'You are the bad', is_audio: false}
+                    ]
+
+RusSentence.create! [
+                        {id: 6, sentence: 'Привет меня зовут Дима', is_audio: false},
+                        {id: 7, sentence: 'Иди домой лол', is_audio: true},
+                        {id: 8, sentence: 'Его автомобиль не очень хороший', is_audio: false},
+                        {id: 9, sentence: 'Я тружусь в одиночестве', is_audio: true},
+                        {id: 10, sentence: 'Ты - кровать', is_audio: false}
+                    ]
