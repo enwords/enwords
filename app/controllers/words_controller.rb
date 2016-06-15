43325bf87873334a7df669a5188bb19c3c74ca28
Dmitry Sadovnikov <d.sadovnikov@yahoo.com>
2016-06-15 17:57:40 +0300

    change word_status methods and fix views

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -9,29 +9,44 @@ class WordsController < ApplicationController
   end
 
   def set_word_status
-    params[:words_ids].each { |word_id|
-      sql = "insert into users_words (word_id, user_id, learned) values (#{word_id}, #{current_user.id}, #{params[:commit]})"
-      ActiveRecord::Base.connection.execute(sql)
-    }
-    redirect_to(:back)
-  end
-
-  def update_word_status
-
-    par = params[:commit]
-    if par == 'Delete'
-
-      UsersWords.delete_all(user_id: current_user, word_id: params[:words_ids])
-     # UsersWords.where(user_id: current_user,
-     #                   word_id: params[:words_ids]).destroy_all
-    else
-      UsersWords.where(user_id: current_user, word_id: params[:words_ids]).update_all(learned: par)
+    parameter = params[:commit]
+    val = []
+    bool = false
+    case parameter
+      when 'Изучать'
+        bool = false
+      when 'Знаю'
+        bool = true
+      else
+        return
     end
-    redirect_to(:back)
-  end
+    params[:words_ids].each { |wid|
+      val << {user_id: current_user.id, word_id: wid, learned: bool}
+    }
+  UsersWords.create! val
+  redirect_to(:back)
+end
 
-  def unset
-    sql = "SELECT words.* FROM words
+def update_word_status
+  parameter = params[:commit]
+  case parameter
+    when 'Скрыть'
+      UsersWords.delete_all(user_id: current_user, word_id: params[:words_ids])
+    else
+      bool = false
+      case parameter
+        when 'Изучать'
+          bool = false
+        when 'Изучил'
+         bool = true
+      end
+      UsersWords.where(user_id: current_user, word_id: params[:words_ids]).update_all(learned: bool)
+  end
+  redirect_to(:back)
+end
+
+def unset
+  sql = "SELECT words.* FROM words
 left join (SELECT users_words.* FROM users_words
 LEFT  JOIN users ON users.id = users_words.user_id
 WHERE (users_words.user_id = #{current_user.id})) as tmp
@@ -39,85 +54,85 @@ on words.id = tmp.word_id
 where tmp.word_id is null
 order by id"
 
-    @words = ActiveRecord::Base.connection.execute(sql)
+  @words = ActiveRecord::Base.connection.execute(sql)
 
-  end
+end
 
-  def learned
-    @words = Word.joins(:users).
-        where(users: {id: current_user}, users_words: {learned: true})
-  end
+def learned
+  @words = Word.joins(:users).
+      where(users: {id: current_user}, users_words: {learned: true})
+end
 
-  def learning
-    @words = Word.joins(:users).
-        where(users: {id: current_user}, users_words: {learned: false})
-  end
+def learning
+  @words = Word.joins(:users).
+      where(users: {id: current_user}, users_words: {learned: false})
+end
 
-  # GET /words/1
-  # GET /words/1.json
-  def show
-  end
+# GET /words/1
+# GET /words/1.json
+def show
+end
 
-  # GET /words/new
-  def new
-    @word = Word.new
-  end
+# GET /words/new
+def new
+  @word = Word.new
+end
 
-  # GET /words/1/edit
-  def edit
-  end
+# GET /words/1/edit
+def edit
+end
 
-  # POST /words
-  # POST /words.json
-  def create
-    word = Word.new(word_params)
+# POST /words
+# POST /words.json
+def create
+  word = Word.new(word_params)
 
-    respond_to do |format|
-      if word.save
-        format.html { redirect_to word, notice: 'Word was successfully created.' }
-        format.json { render :show, status: :created, location: word }
-      else
-        format.html { render :new }
-        format.json { render json: word.errors, status: :unprocessable_entity }
-      end
+  respond_to do |format|
+    if word.save
+      format.html { redirect_to word, notice: 'Word was successfully created.' }
+      format.json { render :show, status: :created, location: word }
+    else
+      format.html { render :new }
+      format.json { render json: word.errors, status: :unprocessable_entity }
     end
   end
+end
 
-  # PATCH/PUT /words/1
-  # PATCH/PUT /words/1.json
-  def update
-    respond_to do |format|
-      if word.update(word_params)
-        format.html { redirect_to word, notice: 'Word was successfully updated.' }
-        format.json { render :show, status: :ok, location: word }
-      else
-        format.html { render :edit }
-        format.json { render json: word.errors, status: :unprocessable_entity }
-      end
+# PATCH/PUT /words/1
+# PATCH/PUT /words/1.json
+def update
+  respond_to do |format|
+    if word.update(word_params)
+      format.html { redirect_to word, notice: 'Word was successfully updated.' }
+      format.json { render :show, status: :ok, location: word }
+    else
+      format.html { render :edit }
+      format.json { render json: word.errors, status: :unprocessable_entity }
     end
   end
+end
 
-  # DELETE /words/1
-  # DELETE /words/1.json
-  def destroy
-    word.destroy
-    respond_to do |format|
-      format.html { redirect_to words_url, notice: 'Word was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+# DELETE /words/1
+# DELETE /words/1.json
+def destroy
+  word.destroy
+  respond_to do |format|
+    format.html { redirect_to words_url, notice: 'Word was successfully destroyed.' }
+    format.json { head :no_content }
   end
+end
 
 
-  private
-  # Use callbacks to share common setup or constraints between actions.
-  def set_word
-    @word = Word.find(params[:id])
-  end
+private
+# Use callbacks to share common setup or constraints between actions.
+def set_word
+  @word = Word.find(params[:id])
+end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def word_params
-    params.require(:word).permit(:id, :word)
-  end
+# Never trust parameters from the scary internet, only allow the white list through.
+def word_params
+  params.require(:word).permit(:id, :word)
+end
 
 
 end
