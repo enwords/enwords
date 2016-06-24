084bcef5e8266f240d52ac20df7c5899cdc97785
Dmitry Sadovnikov <d.sadovnikov@yahoo.com>
2016-06-24 22:05:30 +0300

    refactoring

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,13 +1,9 @@
 class WordsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_word, only: [:show, :edit, :update, :destroy]
 
   # GET /words
   # GET /words.json
   def index
-    # @words = Word.all.order(:id)
-
-
     @words = Word.joins(:sentences).where(sentences: {language_id: current_user.language_1_id}).
         group(:id).order(:id).paginate(page: params[:page], per_page: 20)
   end
@@ -84,84 +80,25 @@ where user_id = #{current_user.id}
     end
   end
 
+  # GET /unset
   def unset
     @words = Word.joins(:sentences).where(sentences: {language_id: current_user.language_1_id}).
         where.not(id: Wordbook.select(:word_id).where(user: current_user)).group(:id).order(:id).paginate(page: params[:page], per_page: 20)
   end
 
+  # GET /learned
   def learned
     @words = current_user.words.where(wordbooks: {learned: true}).order(:id)
   end
 
+  # GET /learning
   def learning
     @words = current_user.words.where(wordbooks: {learned: false}).order(:id)
   end
 
-  # GET /words/1
-  # GET /words/1.json
-  def show
-  end
-
-  # GET /words/new
-  def new
-    @word = Word.new
-  end
-
-  # GET /words/1/edit
-  def edit
-  end
-
-  # POST /words
-  # POST /words.json
-  def create
-    word = Word.new(word_params)
-
-    respond_to do |format|
-      if word.save
-        format.html { redirect_to word, notice: 'Word was successfully created.' }
-        format.json { render :show, status: :created, location: word }
-      else
-        format.html { render :new }
-        format.json { render json: word.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /words/1
-  # PATCH/PUT /words/1.json
-  def update
-    respond_to do |format|
-      if word.update(word_params)
-        format.html { redirect_to word, notice: 'Word was successfully updated.' }
-        format.json { render :show, status: :ok, location: word }
-      else
-        format.html { render :edit }
-        format.json { render json: word.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /words/1
-  # DELETE /words/1.json
-  def destroy
-    word.destroy
-    respond_to do |format|
-      format.html { redirect_to words_url, notice: 'Word was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-
   private
-  # Use callbacks to share common setup or constraints between actions.
-  def set_word
-    @word = Word.find(params[:id])
-  end
-
   # Never trust parameters from the scary internet, only allow the white list through.
   def word_params
     params.require(:word).permit(:id, :word)
   end
-
-
 end
