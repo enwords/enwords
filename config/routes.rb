d0e4b65c1dd4b94427aa4978b5287716f38b8c33
Dmitry Sadovnikov <d.sadovnikov@yahoo.com>
2016-07-03 21:59:39 +0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,23 @@ Rails.application.routes.draw do
   devise_for :users, :skip => [:sessions, :registrations]
 
   as :user do
-    get 'login' => 'devise/sessions#new', :as => :new_user_session
-    post 'login' => 'devise/sessions#create', :as => :user_session
-    delete 'logout' => 'devise/sessions#destroy', :as => :destroy_user_session
+    get 'login' => 'devise/sessions#new', as: :new_user_session
+    post 'login' => 'devise/sessions#create', as: :user_session
+    delete 'logout' => 'devise/sessions#destroy', as: :destroy_user_session
 
-    get 'sign_up' => 'devise/registrations#new', :as => :new_user_registration
-    post 'sign_up' => 'devise/registrations#create', :as => :user_registration
-    get 'settings' => 'devise/registrations#edit', :as => :edit_user_registration
-    delete 'sign_up' => 'devise/registrations#destroy', :as => :destroy_user_registration
+    get 'signup' => 'devise/registrations#new', as: :new_user_registration
+    post 'signup' => 'devise/registrations#create', as: :user_registration
+    put 'signup' => 'devise/registrations#update', as: :update_user_registration
+    delete 'signup' => 'devise/registrations#destroy', as: :destroy_user_registration
+
+    get 'settings' => 'devise/registrations#edit', as: :edit_user_registration
   end
 
   resources :users
 
   resources :sentences
 
-  resources :words   do
+  resources :words do
     collection do
       put :word_action
       put :set_status_on_training
