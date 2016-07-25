0117f5a25e6e709d33032c73268470b1212f2f00
Dmitry Sadovnikov <d.sadovnikov@yahoo.com>
2016-07-25 16:53:38 +0300

    Added pagination and changed style to users page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_action :admin_only, :except => :show
 
   def index
-    @users = User.all
+    @users = User.all.order(:id).paginate(page: params[:page], per_page: 20)
   end
 
   def show
