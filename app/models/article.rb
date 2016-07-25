2125088e39c76ec1b8b3336c0bbe9658ba091f05
Dmitry Sadovnikov <d.sadovnikov@yahoo.com>
2016-07-25 16:54:28 +0300

    Changed articles logic for different languages and changed style

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,9 @@ class Article < ApplicationRecord
   belongs_to :user
   has_many :word_in_articles, dependent: :delete_all
   has_many :words, through: :word_in_articles
+
+  validates :user, presence: true
+  validates :language, presence: true
+  validates :title, presence: true
+  validates :content, presence: true
 end
