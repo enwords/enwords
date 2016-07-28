e363cccaeae839ef8ff21c1e12e1e1f41a76915d
Dmitry Sadovnikov <d.sadovnikov@yahoo.com>
2016-07-28 15:06:42 +0300

    Bug fixing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,20 +4,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_devise_permitted_parameters, if: :devise_controller?
 
-
   protected
   def configure_devise_permitted_parameters
     registration_params = [:email, :password, :password_confirmation,
                            :learning_language, :native_language, :audio_enable,:diversity_enable, :sentences_number]
 
     if params[:action] == 'update'
-      devise_parameter_sanitizer.for(:account_update) {
-          |u| u.permit(registration_params << :current_password)
-      }
+      devise_parameter_sanitizer.permit(:account_update, keys: registration_params << :current_password )
     elsif params[:action] == 'create'
-      devise_parameter_sanitizer.for(:sign_up) {
-          |u| u.permit(registration_params)
-      }
+      devise_parameter_sanitizer.permit(:sign_up, keys: registration_params )
     end
   end
 end
