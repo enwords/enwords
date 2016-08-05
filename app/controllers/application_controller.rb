649f6b9e23a77c3835383b97897c552b1c29d3fa
Dmitry Sadovnikov <d.sadovnikov@yahoo.com>
2016-08-05 15:15:08 +0300

    Translate application to english (now available two languages russian and english)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,18 +3,29 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_devise_permitted_parameters, if: :devise_controller?
+  before_filter :set_locale
+
+  private
+
+  def set_locale
+    I18n.locale = params[:locale] if params[:locale].present?
+    # current_user.locale
+  end
+
+  def default_url_options(options = {})
+     {locale: I18n.locale}
+  end
 
   protected
-
   #Allows to change a profile settings without entering a password
   def configure_devise_permitted_parameters
     registration_params = [:email, :password, :password_confirmation,
-                           :learning_language, :native_language, :audio_enable,:diversity_enable, :sentences_number]
+                           :learning_language, :native_language, :audio_enable, :diversity_enable, :sentences_number]
 
     if params[:action] == 'update'
-      devise_parameter_sanitizer.permit(:account_update, keys: registration_params << :current_password )
+      devise_parameter_sanitizer.permit(:account_update, keys: registration_params << :current_password)
     elsif params[:action] == 'create'
-      devise_parameter_sanitizer.permit(:sign_up, keys: registration_params )
+      devise_parameter_sanitizer.permit(:sign_up, keys: registration_params)
     end
   end
 end
