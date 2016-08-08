8c19b793459d5dbbd2538bdce18a1f308dfd88ed
Dmitry Sadovnikov <d.sadovnikov@yahoo.com>
2016-08-08 10:06:00 +0300

    Added password reset settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,23 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.i18n.default_locale = :en
   config.i18n.fallbacks = true
+
+  # Password reset
+  config.action_mailer.default_url_options = { :host => 'ewords.herokuapp.com' }
+  Rails.application.routes.default_url_options[:host] = 'ewords.herokuapp.com'
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: ENV["GMAIL_DOMAIN"],
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: ENV["GMAIL_USERNAME"],
+      password: ENV["GMAIL_PASSWORD"]
+  }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,36 @@
 Rails.application.routes.draw do
-  resources :users
+
+  # default_url_options :host => "localhost:3000"
+
   as :user do
-    put ':locale/users/sign_up' =>  'devise/registrations#update',  as: :update_user_registration
+    put ':locale/users/sign_up' => 'devise/registrations#update', as: :update_user_registration
+    get ':locale/users/password' => 'devise/passwords#edit', as: :edit_password
   end
 
   scope ":locale", locale: /#{I18n.available_locales.join("|")}/ do
     devise_for :users, controllers: {registrations: 'registrations'}
 
-     root to: 'static#index'
-     put '/words' => 'words#index'
-     get '/training' => 'words#training', as: :training
-     get '/result' => 'static#result'
+    root to: 'static#index'
+    put '/words' => 'words#index'
+    get '/training' => 'words#training', as: :training
+    get '/result' => 'static#result'
 
+    resources :users
+    resources :sentences
+    resources :articles do
+      collection do
+        put :delete_articles
+      end
+    end
 
-     resources :sentences
-     resources :articles do
-       collection do
-         put :delete_articles
-       end
-     end
-
-     resources :words do
-       collection do
-         put :word_action
-         post :set_word_status_training
-       end
-     end
-   end
+    resources :words do
+      collection do
+        put :word_action
+        post :set_word_status_training
+      end
+    end
+  end
 
   match '*path', to: redirect("/#{I18n.default_locale}/%{path}%"), via: [:get]
-  match '', to: redirect("/#{I18n.default_locale}") , via: [:get]
+  match '', to: redirect("/#{I18n.default_locale}"), via: [:get]
 end
