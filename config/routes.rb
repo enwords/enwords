ac62a42c9ea5d022314d8736759f0604b1a3e3f0
Dmitry Sadovnikov <d.sadovnikov@yahoo.com>
2016-08-10 16:41:38 +0300

    Added spelling train

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
     root to: 'static#index'
     put '/words' => 'words#index'
     get '/training' => 'words#training', as: :training
+    get '/training_spelling' => 'words#training_spelling', as: :training_spelling
     get '/result' => 'static#result'
 
     resources :users
