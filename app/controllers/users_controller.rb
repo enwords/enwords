1705b98e9a0b56e2d194f2aa76dc42cf83e7961e
Dmitry Sadovnikov <d.sadovnikov@yahoo.com>
2016-08-11 11:48:24 +0300

    Fixed bugs and refactored

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     unless current_user.admin?
-      unless @user == current_user
         redirect_to :back, :alert => "Access denied."
-      end
     end
   end
 
