b7e687b9541ce1bfe5b72d2cd503df47c07c2709
Dmitry Sadovnikov <d.sadovnikov@yahoo.com>
2016-08-21 13:28:48 +0300

    Reorder columns for csv import

--- db/migrate/20160608075034_create_words.rb
+++ db/migrate/20160608075034_create_words.rb
@@ -2,8 +2,8 @@ class CreateWords < ActiveRecord::Migration[5.0]
   def change
     create_table(:words, :id => false) do |t|
       t.integer :id, :primary_key => true, :unique => true , :null => false
-      t.string :word, unique: true
       t.string :language, limit: 4
+      t.string :word, unique: true
     end
   end
 end
