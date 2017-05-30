b1ae4c0be717f0358bde041972772b53c70c67e9
Dmitry Sadovnikov <sadedv@gmail.com>
2017-05-30 23:07:31 +0300

    [training] Add hotkeys for training (arrows)

---
+++ app/assets/javascripts/separated/repeating.js
@@ -0,0 +1,29 @@
+$(function () {
+  'use strict';
+
+  var $previousPage = $('.previous_page');
+  var $nextPage     = $('.next_page');
+  var $linkResult   = $('#link-result');
+
+  $previousPage.on('click', onclickRedirect);
+  $nextPage.on('click', onclickRedirect);
+  $linkResult.on('click', onclickRedirect);
+
+  function onclickRedirect() {
+    if ($(this).attr('href') !== undefined) {
+      window.location = $(this).attr('href');
+    }
+  }
+
+  $(document).keyup(function (e) {
+    switch (e.which) {
+      case 37:
+        $previousPage.trigger('click');
+        break;
+      case 39:
+        $linkResult.trigger('click');
+        $nextPage.trigger('click');
+        break;
+    }
+  });
+});
--- app/assets/javascripts/separated/training.js
+++ app/assets/javascripts/separated/training.js
@@ -1,18 +1,31 @@
 $(function () {
   'use strict';
 
+  var $btnShowTranslation = $('#btn-show-translation');
+
   $.ajax({
     url: '/words_from_sentence/' + $('#original-sentence').data('sentence-id'),
   }).done(function (data) {
     $('.words-from-sentence-before').replaceWith(data);
   });
 
-  $('#btn-show-translation').on('click', function () {
+  $btnShowTranslation.on('click', function () {
     $(this).fadeOut(0);
     $('#translation').fadeIn(750);
   });
 
   $('#btn-play-audio').on('click', function () {
     $(this).children('audio')[0].play();
+  });
+
+  $(document).keyup(function (e) {
+    switch (e.which) {
+      case 40:
+        $btnShowTranslation.trigger('click');
+        break;
+      case 38:
+        $('#btn-play-audio').trigger('click');
+        break;
+    }
   });
 });
