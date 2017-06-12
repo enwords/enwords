f7a88cb43e3441f5dc9b5f5030e541fe26f8e019
Dmitry Sadovnikov <sadedv@gmail.com>
2017-06-12 20:32:07 +0300

    [js] Delete extra js

--- app/assets/javascripts/separated/training.js
+++ app/assets/javascripts/separated/training.js
@@ -7,6 +7,25 @@ $(function () {
     url: '/words_from_sentence/' + $('#original-sentence').data('sentence-id'),
   }).done(function (html) {
     $('.words-from-sentence-before').replaceWith(html);
+    $.getScript('/assets/skyeng.js');
+
+    $('.words-from-sentence-after').hide().fadeIn(500);
+
+    // switcher
+    var $switcher = $('.switcher');
+
+    $('[data-status="true"]').addClass('switcher_learned');
+    $('[data-status="unknown"]').addClass('switcher_unknown');
+
+    $switcher.on('click', function () {
+      $.ajax({
+        url: '/change_status/' + $(this).data('id') + '/' + $(this).attr('data-status'),
+      }).done(function (responce) {
+        var $thisButton = $('[data-id=' + responce[0].word_id + ']');
+        $thisButton.attr('data-status', responce[0].learned);
+        $thisButton.removeClass('switcher_unknown').toggleClass('switcher_learned');
+      });
+    });
   });
 
   $btnShowTranslation.on('click', function (e) {
