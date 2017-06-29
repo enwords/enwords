8bcfbec42dd1a3a72b249880e8478d73d9e94d80
Dmitry <sadedv@gmail.com>
2017-06-29 22:21:20 +0300

    [deploy] Delet bin dir from simlink

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, 'git@github.com:enwords/enwords.git'
 set :deploy_to, '/home/deploy/enwords'
 
 set :linked_files, %w[config/database.yml config/secrets.yml config/application.yml]
-set :linked_dirs, %w[bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system]
+set :linked_dirs, %w[log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system]
 
 namespace :deploy do
 
