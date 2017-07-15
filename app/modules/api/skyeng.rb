f62327f33a148d52713ccd98a3ccf45a22e58d33
Dmitry <sadedv@gmail.com>
2017-07-15 13:40:22 +0300

    [skyeng] Add others Api of skyeng

---
+++ app/modules/api/skyeng.rb
@@ -0,0 +1,49 @@
+module Api
+  module Skyeng
+    class << self
+      def send_token(email:)
+        build_get_response('https://words.skyeng.ru/api/public/v1/users/token',
+                           email: email)
+      end
+
+      def learning_word_ids(email:, token:)
+        result = build_get_response('https://words.skyeng.ru/api/public/v1/users/meanings',
+                                    email: email,
+                                    token: token)
+
+        return :invalid_params unless result.is_a?(Array)
+        result.map { |el| el['meaningId'] }
+      end
+
+      def words(ids:)
+        result = build_get_response('http://dictionary.skyeng.ru/api/public/v1/meanings',
+                                    ids: ids.join(','))
+
+        return :invalid_params unless result.is_a?(Array)
+        result.map { |i| i['text'] }.uniq
+      end
+
+      def first_meaning(word:)
+        result = build_get_response('http://dictionary.skyeng.ru/api/public/v1/words/search',
+                                    search: word)
+
+        return :invalid_params unless result.is_a?(Array)
+        return {} if result.blank?
+
+        meanings = result.dig(0, 'meanings').sort_by { |el| el['id'] }
+        meanings[0].merge('text' => result.dig(0, 'text'))
+      end
+
+      def build_get_response(url, params = {})
+        uri       = URI(url)
+        uri.query = URI.encode_www_form(params)
+        response  = Net::HTTP.get_response(uri)
+
+        return :ok unless response.body
+        JSON.parse(response.body)
+      rescue
+        :fail
+      end
+    end
+  end
+end
