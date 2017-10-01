e978ad0a9a22419d2422acb84937311fc8d915a4
Dmitry Sadovnikov <sadovnikov.js@gmail.com>
2017-10-01 20:05:51 +0300

    [global] Cleanup

--- app/controllers/skyeng_controller.rb
+++ app/controllers/skyeng_controller.rb
@@ -1,5 +1,5 @@
 class SkyengController < ApplicationController
   def first_meaning
-    render json: Api::Skyeng.first_meaning(word: params[:word].downcase)
+    render json: Api::Skyeng.first_meaning(word: params[:word])
   end
 end
--- app/services/api/skyeng.rb
+++ app/services/api/skyeng.rb
@@ -1,65 +1,71 @@
 module Api
   module Skyeng
-    class << self
-      def send_token(email:)
-        build_get_response('https://words.skyeng.ru/api/public/v1/users/token',
-                           email: email)
-      end
+    module_function
 
-      def learning_word_ids(email:, token:)
-        result = build_get_response('https://words.skyeng.ru/api/public/v1/users/meanings',
-                                    email: email,
-                                    token: token)
+    def send_token(email:)
+      build_get_response 'https://words.skyeng.ru/api/public/v1/users/token',
+                         email: email
+    end
 
-        return :invalid_params unless result.is_a?(Array)
-        result.map { |el| el['meaningId'] }
-      end
+    def learning_word_ids(email:, token:)
+      result = build_get_response \
+        'https://words.skyeng.ru/api/public/v1/users/meanings',
+        email: email,
+        token: token
 
-      def words(ids:)
-        threads = []
-        result  = []
+      return :invalid_params unless result.is_a?(Array)
+      result.map { |el| el['meaningId'] }
+    end
 
-        ids.each_slice(100) do |sliced_ids|
-          threads << Thread.new do
-            result << build_get_response('http://dictionary.skyeng.ru/api/public/v1/meanings',
-                                         ids: sliced_ids.join(','))
-          end
+    def words(ids:)
+      threads = []
+      result  = []
+
+      ids.each_slice(100) do |sliced_ids|
+        threads << Thread.new do
+          result << build_get_response(
+            'http://dictionary.skyeng.ru/api/public/v1/meanings',
+            ids: sliced_ids.join(',')
+          )
         end
-
-        threads.each(&:join)
-        result.flatten.flat_map { |i| i['text'].split }.uniq
       end
 
-      def learning_words(email:, token:)
-        ids = learning_word_ids(email: email, token: token)
-        return [] unless ids.is_a?(Array)
-        words(ids: ids)
-      end
+      threads.each(&:join)
+      result.flatten.flat_map { |i| i['text'].split }.uniq
+    end
 
-      def first_meaning(word:)
-        result = build_get_response('http://dictionary.skyeng.ru/api/public/v1/words/search',
-                                    search: word)
+    def learning_words(email:, token:)
+      ids = learning_word_ids(email: email, token: token)
 
-        return :invalid_params unless result.is_a?(Array)
-        return {} if result.blank?
+      return [] unless ids.is_a?(Array)
+      words(ids: ids)
+    end
 
-        word_block = result.find { |i| word.casecmp(i['text']).zero? }
-        return {} if word_block.blank?
+    def first_meaning(word:)
+      word   = word.gsub(/\W/, '').downcase
+      result = build_get_response \
+        'http://dictionary.skyeng.ru/api/public/v1/words/search',
+        search: word
 
-        meanings = word_block['meanings'].sort_by { |el| el['id'] }
-        meanings[0].merge('text' => word_block['text'])
-      end
+      return :invalid_params unless result.is_a?(Array)
+      return {} if result.blank?
 
-      def build_get_response(url, params = {})
-        uri       = URI(url)
-        uri.query = URI.encode_www_form(params)
-        response  = Net::HTTP.get_response(uri)
+      word_block = result.find { |i| word.casecmp(i['text']).zero? }
+      return {} if word_block.blank?
 
-        return :ok unless response.body
-        JSON.parse(response.body)
-      rescue
-        :fail
-      end
+      meanings = word_block['meanings'].sort_by { |el| el['id'] }
+      meanings[0].merge('text' => word_block['text'])
+    end
+
+    def build_get_response(url, params = {})
+      uri       = URI(url)
+      uri.query = URI.encode_www_form(params)
+      response  = Net::HTTP.get_response(uri)
+
+      return :ok unless response.body
+      JSON.parse(response.body)
+    rescue
+      :fail
     end
   end
 end
