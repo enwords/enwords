f6522fcdbf77e7e9b45357b3b9b435d6b4ae8ee5
Dmitry Sadovnikov <sadovnikov.js@gmail.com>
2017-10-29 12:27:16 +0300

    [mail] Add new email template

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,8 +1,12 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'support@enwords.tk'
+  default from: "Enwords<#{ENV['GMAIL_USERNAME']}>"
   layout 'mailer'
 
   def test_email(email)
     mail(to: email, subject: 'Welcome to My Awesome Site', body: 'Хорошего дня')
+  end
+
+  def feedback_mail(email)
+    mail(to: email, subject: 'Нам нужен твой отзыв')
   end
 end
