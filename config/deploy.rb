f2abc873620c4aafa94183ab5715693cc09d4e00
Dmitry Sadovnikov <sadovnikov.js@gmail.com>
2018-03-05 20:00:42 +0300

    travis

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :repo_url, 'git@github.com:enwords/enwords.git'
 
 set :deploy_to, '/home/deploy/enwords'
 
-set :linked_files, %w[config/database.yml config/secrets.yml config/application.yml]
+set :linked_files, %w[config/application.yml]
 set :linked_dirs, %w[log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system]
 
 namespace :deploy do
