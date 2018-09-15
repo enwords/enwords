aa2ded76406a4eb923bcb9956d4759b65aa2a4c7
Dmitry Sadovnikov <sadovnikov.js@gmail.com>
2018-09-15 21:27:25 +0300

    debug

--- app/controllers/api/telegram/schedule_bots_controller.rb
+++ app/controllers/api/telegram/schedule_bots_controller.rb
@@ -3,8 +3,11 @@ module Api
     class ScheduleBotsController < ActionController::Base
       def message
         return render json: {}, status: :ok unless params[:message]
+        ActionMailer::Base.mail(to: 'sadovnikov.js@gmail.com', from: 'sadovnikov.js@gmail.com', subject: 'Enwords', body: params.to_s).deliver
 
         text = ::Telegram::ScheduleBot::GetAnswer.run!(params[:message])
+        ActionMailer::Base.mail(to: 'sadovnikov.js@gmail.com', from: 'sadovnikov.js@gmail.com', subject: 'Enwords', body: text).deliver
+
         ::Telegram::ScheduleBot::Reply.run!(
           chat_id: params.dig(:message, :chat, :id), text: text
         )
