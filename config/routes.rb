feaea5234087fb976d99261ae9344a3f5ce0dde2
Dmitry Sadovnikov <sadovnikov.js@gmail.com>
2018-09-15 21:53:47 +0300

    try

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       resources :schedule_bots, only: %i[] do
         collection do
           post :message, defaults: { format: :json }
+          get  :message, defaults: { format: :json }
         end
       end
     end
