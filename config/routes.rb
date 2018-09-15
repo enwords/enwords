ceaf47c5cc2dfcad516639275c643423a60c2091
Dmitry Sadovnikov <sadovnikov.js@gmail.com>
2018-09-15 21:09:11 +0300

    post

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace :telegram do
       resources :schedule_bots, only: %i[] do
         collection do
-          get :message
+          post :message
         end
       end
     end
