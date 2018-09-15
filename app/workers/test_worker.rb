85c071e7e09787839ced9ae8aed9d0f45b003640
Dmitry Sadovnikov <sadovnikov.js@gmail.com>
2018-09-15 20:47:24 +0300

    coment worker

--- app/workers/test_worker.rb
+++ app/workers/test_worker.rb
@@ -4,7 +4,7 @@ class TestWorker
   include Sidekiq::Worker
 
   def perform(*)
-    ApplicationMailer.test_email('sadovnikov.js@gmail.com').deliver
+    # ApplicationMailer.test_email('sadovnikov.js@gmail.com').deliver
     :ok
   end
 end
