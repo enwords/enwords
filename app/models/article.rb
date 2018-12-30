603d545dd39460456aef503cc20185df8e56e577
Dmitry Sadovnikov <sadovnikov.js@gmail.com>
2018-12-30 14:24:54 +0300

    fix

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ApplicationRecord
   belongs_to :user, optional: true
 
   validates :language, :title, :content, presence: true
-  before_save :word_frequency
+  before_save :calc_word_frequency
 
   store_accessor :words_data,
                  :frequency,
@@ -10,23 +10,14 @@ class Article < ApplicationRecord
 
   private
 
-  def word_frequency
-    text            = self.content.downcase.gsub(/[[:punct:]\d\+\$\^\=\–\>\<\~\`\№]/, '')
-    words           = text.split(' ')
-    words_from_text = words.each_with_object({}) do |word, frequencies|
-      begin
-        frequencies[word] += 1
-      rescue
-        frequencies[word] = 1
-      end
+  def calc_word_frequency
+    text          = content.downcase.gsub(/[[:punct:]\d\+\$\^\=\–\>\<\~\`\№]/, '')
+    words         = text.split(' ')
+    grouped_words = words.each_with_object(Hash.new(0)) { |e, h| h[e] += 1 }
+    db_words      = Word.where(word: words, language: language)
+
+    words_data['frequency'] = db_words.each_with_object({}) do |word, hsh|
+      hsh[word.id] = grouped_words[word.word]
     end
-
-    words_from_db  = Word.where(word: words_from_text.map(&:first), language: self.language)
-
-    xx = words_from_db.each_with_object({}) do |word, hsh|
-      hsh[word.id] = words_from_text[word.word]
-    end
-
-    self.words_data['frequency'] = xx
   end
 end
