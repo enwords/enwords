3356f0f592f2dfc174ca9ca5a6cef06a931eea6a
Dmitry Sadovnikov <sadovnikov.js@gmail.com>
2019-01-07 13:16:57 +0300

    fix

--- app/workers/test_worker.rb
+++ app/workers/test_worker.rb
@@ -6,23 +6,24 @@ class TestWorker
   def perform(*)
     user           = User.find(1)
     learning_words = Word::ByStatus.run!(status: 'learning', user: user)
-    word           = learning_words.sample.value
-    translation    = skyeng_translation(word)
+    word           = learning_words.sample
+    word_value     = word.value
+    translation    = skyeng_translation(word_value)
     return :no_translation unless translation
 
-    text = Sentence::ByWord.run!(word: word, language: 'eng', translation_language: 'rus').text
+    text = Sentence::ByWord.run!(word: word, trans_lang: 'rus').text
 
     ActionMailer::Base.mail(
       from:    ENV['GMAIL_USERNAME'],
       to:      ENV['GMAIL_USERNAME'],
-      subject: word + ' - ' + translation,
+      subject: word_value + ' - ' + translation,
       body:    text
     ).deliver
 
     :ok
   end
 
-  def skyeng_translation(word)
-    Api::Skyeng.first_meaning(word: word).dig('translation', 'text') rescue nil
+  def skyeng_translation(word_value)
+    Api::Skyeng.first_meaning(word: word_value).dig('translation', 'text') rescue nil
   end
 end
