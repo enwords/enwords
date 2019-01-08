146730e47683d31ecf5ef35fe933260dcc1145b8
Dmitry Sadovnikov <sadovnikov.js@gmail.com>
2019-01-08 21:47:49 +0300

    fix

--- app/services/telegram/schedule_bot/get_answer.rb
+++ app/services/telegram/schedule_bot/get_answer.rb
@@ -27,6 +27,7 @@ module Telegram
         when /xoxo/i
           'YOLO!'
         else
+          return unless word
           Sentence::ByWord.run!(word: word, trans_lang: trans_lang).text
         end
       end
