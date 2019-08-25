870700292b0f7fed40532730ff0b4669a103ad65
Dmitry Sadovnikov <sadovnikov.js@gmail.com>
2019-08-26 01:22:13 +0300

    Change tg text format

--- app/services/sentence/by_word.rb
+++ app/services/sentence/by_word.rb
@@ -1,43 +1,72 @@
 class Sentence < ApplicationRecord
   class ByWord < ActiveInteraction::Base
+    private
+
     object :word
     string :trans_lang
 
     def execute
       OpenStruct.new(
-        sentence:    sentence.try(:value),
-        translation: translation.try(:value),
-        text:        text
+        word: word.value,
+        sentence: sentence.try(:value),
+        word_translation: word_translation,
+        sentence_translation: sentence_translation,
+        menemo: word_mnemo,
+        text: text
       )
     end
 
     def sentence
-      @_sentence ||=
-        word
-        .sentences
-        .left_joins(:translations)
-        .where(translations_sentences: { language: trans_lang })
-        .sample
+      @sentence ||= word.sentences.left_joins(:translations).where(translations_sentences: { language: trans_lang }).sample
     end
 
-    def translation
-      @_translation ||=
-        sentence
-        .translations
-        .where(language: trans_lang)
-        .first
+    def sentence_translation
+      @sentence_translation ||= sentence.translations.where(language: trans_lang).first.try(:value)
+    end
+
+    def skyeng_hash
+      @skyeng_hash ||= Api::Skyeng.first_meaning(word: word.value) || {} rescue {}
+    end
+
+    def word_translation
+      skyeng_hash.dig('translation', 'text') || ''
+    end
+
+    def word_transcription
+      skyeng_hash['transcription']
+    end
+
+    def word_mnemo
+      @word_mnemo ||= word.mnemos.first.try(:value)
     end
 
     def text
       return unless sentence.try(:value)
 
-      <<~HEREDOC
-        #{sentence.try(:value)}
-        * * *
-        #{translation.try(:value)}
-        * * *
-        #{word.mnemos.map(&:value).join("\n")}
-      HEREDOC
+      result = ''
+      result << word.value
+      result << ' '
+      result << "[#{word_transcription}] " if word_transcription.present?
+      result << '- '
+      result << word_translation
+      result << "\n"
+      result << "\n"
+      result << sentence.value
+
+      if sentence_translation.present?
+        result << "\n"
+        result << "-"
+        result << "\n"
+        result << sentence_translation
+      end
+
+      if word_mnemo.present?
+        result << "\n"
+        result << "\n"
+        result << word_mnemo
+      end
+
+      result
     end
   end
 end
--- app/workers/test_worker.rb
+++ app/workers/test_worker.rb
@@ -1,27 +1,17 @@
-# frozen_string_literal: true
-
 class TestWorker
   include Sidekiq::Worker
 
   def perform(*)
-    user           = User.find(1)
+    user = User.find(1)
     learning_words = Word::ByStatus.run!(status: 'learning', user: user)
-    word           = learning_words.sample
-    word_value     = word.value
-    translation    = skyeng_translation(word_value)
-    return :no_translation unless translation
+    word = learning_words.sample
+    return :no_word unless word
 
-    text = Sentence::ByWord.run!(word: word, trans_lang: 'rus').text
+    sentence_struct = Sentence::ByWord.run!(word: word, trans_lang: 'rus')
+    word_translation = sentence_struct.text
+    return :no_word_translation unless word_translation
 
-    Telegram::ScheduleBot::Reply.run!(
-      text: word_value + ' - ' + translation + "\n" + text,
-      chat_id: 160589750
-    )
-
+    Telegram::ScheduleBot::Reply.run!(text: sentence_struct.text, chat_id: 160_589_750)
     :ok
-  end
-
-  def skyeng_translation(word_value)
-    Api::Skyeng.first_meaning(word: word_value).dig('translation', 'text') rescue nil
   end
 end
