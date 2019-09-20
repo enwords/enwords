819247c0e80db58d56df7bc25b36ed9d2cc229bb
Dmitry Sadovnikov <sadovnikov.js@gmail.com>
2019-09-20 23:35:59 +0300

    Delete proxy

--- app/services/telegram/base_request.rb
+++ app/services/telegram/base_request.rb
@@ -3,11 +3,7 @@ module Telegram
     URL = "https://api.telegram.org/bot#{ENV['TELEGRAM_BOT_KEY']}".freeze
 
     def response(path, options = {})
-      result = Curl.post(URL + '/' + path, options) do |curl|
-        curl.proxy_tunnel = true
-        curl.proxy_url = ENV['PROXY_URL']
-        curl.proxypwd = ENV['PROXY_PWD']
-      end
+      result = Curl.post(URL + '/' + path, options)
       JSON.parse(result.body).deep_symbolize_keys
     end
   end
